4556065b1833b8afa606dcaf6423f484056863b6
Jaykimani <josephkimani1998@gmail.com>
2021-12-04 10:57:14 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -9,7 +9,8 @@
    preloadTl.to(".nobu", {stagger: 0.1, color:"#fff708", duration: 0.1}, ">-0.1");
    preloadTl.to(".preloader",{ zIndex: -5,opacity: 0, duration:0.3, delay: 0.3});
    preloadTl.from(".content-sec h1", {x: 40, opacity: 0, duration: 0.5});
-   preloadTl.from(".content-sec p", { x: -40, opacity:0, duration: 0.5});
+   preloadTl.from(".content-sec h1 span",{opacity: 0, duration: 0.1, repeat :5});
+   preloadTl.from(".content-sec p", { x: -40, opacity:0, duration: 0.5, delay: 0.2});
    preloadTl.from(".content-sec button", {y: 40, opacity: 0, duration: 0.5});
 
 // navbar gsap
@@ -23,13 +24,9 @@
    const navSpan3 = document.querySelector(`.nav-toggle span:nth-child(${c})`);
    const navLinks = document.querySelectorAll(".nav-link");
 
-   // if($(window).width() < 450){
-   //    navToggle.addEventListener("click", function(){
-   //       navTl.from(navContent, {x: 300, duration: 0.2});
-   //    });
-   // }
+   
    var navTl = gsap.timeline({ defaults: {duration: 1, delay: 0}});
-   navTl.from(navContent, {x: 300, duration: 0.2});
+   navTl.from(navContent, {x: 330, duration: 0.2});
    navTl.to(navSpan2, {opacity: 0, duration: 0.2}, ">-0.3");
    navTl.to(navSpan1, {transform: "rotate(45deg)", y: 11, duration: 0.3}, ">-0.2");
    navTl.to(navSpan3,{transform: "rotate(-45deg)", y: -12, duration: 0.3}, ">-0.3");
@@ -39,18 +36,18 @@
    
    navToggle.addEventListener("click", function(){
 
-      if(navContent.classList.contains("is-active") && $(window).width() > 500){
-         navToggle.style.transform = "translateX(-230px)";
+      if(navContent.classList.contains("is-active") && $(window).width() > 610){
+         navToggle.style.transform = "translateX(-260px)";
          navTl.play();
          gsap.from(".nav-link", {stagger: 0.2, x: 30, opacity: 0, duration: 0.5});
          navContent.classList.remove("is-active");
-      } else if(navContent.classList.contains("is-active") && $(window).width() <= 500){
-         navToggle.style.transform = "translateX(-130px)";
+      } else if(navContent.classList.contains("is-active") && $(window).width() < 610){
+         navToggle.style.transform = "translateX(-180px)";
          navTl.play();
          gsap.from(".nav-link", {stagger: 0.2, x: 30, opacity: 0, duration: 0.5});
          navContent.classList.remove("is-active");
       } else if(navContent.classList.contains("is-active") && $(window).width() <= 360){
-         navToggle.style.transform = "translateX(-100px)";
+         navToggle.style.transform = "translateX(-150px)";
          navTl.play();
          gsap.from(".nav-link", {stagger: 0.2, x: 30, opacity: 0, duration: 0.5});
          navContent.classList.remove("is-active");
@@ -71,6 +68,17 @@
     });
    });
 
+   // about section
+   gsap.from(".about-img-content", {
+      scrollTrigger:{
+         trigger: ".about-sec",
+         start: "top 50%"
+      },
+      opacity: 0,
+      duration: 0.1, 
+      repeat :15
+
+   })
    
 
    // main menu  
@@ -79,8 +87,10 @@
    const menuImage = document.querySelectorAll(".menu-img");
    const outerMenu = document.querySelector(".outer-menu");
    const lunch = document.querySelector(".lunch").innerHTML;
+   const lunchImg = document.querySelector(".lunch-img");
 
    menuSelected.innerHTML = lunch;
+
  
   
 
@@ -98,6 +108,7 @@
    
    menuSelect.forEach(function(menu){
      menu.addEventListener("click", function(e){
+      lunchImg.style.zIndex = "-4";
       let selected =e.currentTarget.innerHTML.toLowerCase();
       let selectedDiv = outerMenu.querySelector(`.${selected}`);
       
@@ -138,9 +149,6 @@
       testTl.to(test , {transform: "rotate(3deg)"});
       testTl.to(test , {transform: "rotate(0)"});
 
-      if($(window).width() < 960){
-         testTl.pause();
-      }
     });
  });
 
@@ -157,14 +165,14 @@ teamImg.forEach(function(team){
     var teamTl = gsap.timeline();
     teamTl.to(`#${target} .team-info`, {y: -100, duration: 0.5, ease: "power4"});
      
-    if($(window).width() < 960){
+    if($(window).width() < 610){
        teamTl.pause();
     }
     team.addEventListener("mouseleave", function(){
        var team = gsap.timeline();
       team.to(`#${target} .team-info`, {y: -50, duration: 1, ease: "power4"});
 
-      if($(window).width() < 960){
+      if($(window).width() < 610){
          team.pause();
       }
     });
